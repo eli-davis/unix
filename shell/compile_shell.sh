c872ca43e7e2abae2d62ef8890a2ac23ba8c5c52
Eliyahu Davis <elidavis@me.com>
2015-03-06 03:28:23 -0500

    Update and rename shell to shell/compile_shell.sh

---
+++ shell/compile_shell.sh
@@ -0,0 +1,13 @@
+#!/bin/tcsh
+
+echo "cc -c shell_command_line.c"
+cc -c shell_command_line.c
+if ($status != 0) exit
+
+echo "cc -c shell_main.c"
+cc -c shell_main.c
+if ($status != 0) exit
+
+echo "cc -g shell_command_line.o shell_main.o -o program"
+cc -g shell_command_line.o shell_main.o -o launch_shell
+if ($status != 0) exit
