797159afa096fbbe3a7fe43ddadad07666249e95
Eliyahu Davis <elidavis@me.com>
2015-03-06 03:33:11 -0500

    Update shell_main.c

--- shell/shell_main.c
+++ shell/shell_main.c
@@ -5,24 +5,24 @@
 
 typedef int bool;
 
-void print_commands()
+void print_user_commands()
 { printf("\nCommands: \n");
   printf("  help \n");
   printf("  echo \n");
   printf("  q/quit/exit/logout \n");
   printf("  run executable_name arg1 arg2 arg3 \n\n"); }
 
-bool is_quit_command(char * argument)
+bool quit_command(char * argument)
 { int quit = 0;
   if ( !strcmp(argument, "q") || !strcmp(argument, "quit") ||
        !strcmp(argument, "exit") || !strcmp(argument, "logout") )
     quit = 1;
   return quit; }
 
-void run_program(char * * s, int n_args)
+void run_program(char * * s, int argc)
 { int i, pid, pid2, status;
-  char * args[max_arg_c];
-  for (i = 0; i < n_args; i += 1)
+  char * args[max_argc];
+  for (i = 0; i < argc; i += 1)
     args[i] = strdup(s[i+1]);
   args[i-1] = NULL;
   pid = fork();
@@ -32,30 +32,36 @@ void run_program(char * * s, int n_args)
     exit(1); }
   pid2 = wait(& status); }
 
+void exit_shell(char * * string_array)
+{ int i;
+  printf("exiting shell \n");
+  for (i = 0; i < max_argc; i += 1)
+    free(string_array[i]);
+  free(string_array); }
+
+void echo_commands(char * * string_array, int argc)
+{ int i;
+  for (i = 1; i < argc && string_array[i]; i += 1)
+  printf("%s ", string_array[i]); }
+
 void simple_shell()
-{ int n_args, i;
-  char * * sarray = malloc(max_arg_c * sizeof(*sarray));
-  for (i = 0; i < max_arg_c; i += 1)
-    sarray[i] = malloc(max_arg_length + 1);
-  print_commands();
+{ int i, argc;
+  char * * string_array = malloc(max_argc * sizeof(*string_array));
+  for (i = 0; i < max_argc; i += 1)
+    string_array[i] = malloc(max_arg_length + 1);
+  print_user_commands();
   while (1)
   { printf("\n---> ");
-    for (i = 0; i < max_arg_c; i += 1)
-      *sarray[i] = 0;
-    n_args = get_command_line(sarray);
-    if (is_quit_command(sarray[0]))
-    { printf("exiting shell \n");
-      for (i = 0; i < max_arg_c; i += 1)
-        free(sarray[i]);
-      free(sarray);
+    argc = get_command_line(string_array);
+    if (quit_command(string_array[0]))
+    { exit_shell(string_array);
       break; }
-    else if (!strcmp(sarray[0], "help"))
-      print_commands();
-    else if (!strcmp(sarray[0], "echo"))
-    { for (i = 1; i < n_args && sarray[i] != NULL; i += 1)
-        printf("%s ", sarray[i]); }
-    else if (!strcmp(sarray[0], "run"))
-      run_program(sarray, n_args);
+    else if (!strcmp(string_array[0], "help"))
+      print_user_commands();
+    else if (!strcmp(string_array[0], "echo"))
+      echo_commands(string_array, argc);
+    else if (!strcmp(string_array[0], "run"))
+      run_program(string_array, argc);
     else
       printf("command not found. "); } }
 
