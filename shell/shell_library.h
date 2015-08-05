333616df064eeff44d83a2df73fb2a50f28360f5
Eliyahu Davis <elidavis@me.com>
2015-08-04 21:22:19 -0500

    Update shell_library.h

--- shell/shell_library.h
+++ shell/shell_library.h
@@ -1,5 +1,7 @@
-extern const int max_argc;
+extern const int max_arg_c;
 extern const int max_arg_length;
 extern const int buffer_size;
 
+char * process_string(char * input);
 int get_command_line(char * * array_of_strings);
+
