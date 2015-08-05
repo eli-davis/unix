192258682b29e9a9958d1f1ace0fb60db591313a
Eliyahu Davis <elidavis@me.com>
2015-08-04 21:28:30 -0500

    Update command_line.c

--- shell/command_line.c
+++ shell/command_line.c
@@ -2,7 +2,6 @@
 #include <stdio.h>
 #include <string.h>
 
-
 const int max_arg_c = 16;
 const int max_arg_length = 16;
 const int buffer_size = 256;
@@ -24,16 +23,13 @@ char * process_string(char * input)
 { int i;
   char * local_string, * temp, * result;
   static char * static_string = NULL;
-
   if (input != NULL)
     static_string = input;
   if (static_string == NULL)
     return NULL;
-
   while (static_string[0] == ' ')
     static_string += 1;
   local_string = static_string;
-
   if (local_string[0] == '\"')
   { static_string += 1;
     local_string += 1;
@@ -47,28 +43,22 @@ char * process_string(char * input)
     { if (escape_char(local_string))
         static_string = handle_escape_char(static_string, local_string);
       local_string += 1; } }
-
   result = static_string;
-
   if (local_string[0] == '\0')
     static_string = NULL;
   else
   { local_string[0] = '\0';
     static_string = local_string + 1; }
-
  return result;  }
  
-
 int get_command_line(char * * array_of_strings)
 { int i, arg_c;
   char buffer[256];
-
   for (i = 0; i < buffer_size; i += 1)
   { buffer[i] = getchar();
     if (buffer[i] == '\n' || i == buffer_size-1)
     { buffer[i] = '\0';
       break; } }
-
   strcpy(array_of_strings[0], process_string(buffer));
   for (arg_c = 1; array_of_strings[arg_c-1] != NULL && arg_c < max_arg_c; arg_c += 1)
   { char * s = process_string(NULL);
@@ -76,5 +66,4 @@ int get_command_line(char * * array_of_strings)
       strcpy(array_of_strings[arg_c], s);
     else
       break; }
-      
   return arg_c; }
