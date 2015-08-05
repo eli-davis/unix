3ee37291ea14e33ad9fc4dc5eefd89c5282ed948
Eliyahu Davis <elidavis@me.com>
2015-08-04 21:18:13 -0500

    Update compile_shell.sh

--- shell/compile_shell.sh
+++ shell/compile_shell.sh
@@ -9,5 +9,8 @@ cc -c shell_main.c
 if ($status != 0) exit
 
 echo "cc -g shell_command_line.o shell_main.o -o program"
-cc -g shell_command_line.o shell_main.o -o launch_shell
+cc -g shell_command_line.o shell_main.o -o shell
 if ($status != 0) exit
+
+echo "running shell \n\n"
+./shell
